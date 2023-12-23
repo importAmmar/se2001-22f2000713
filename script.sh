1665dc7ef8b3e0b131c0ac68540f7c21bdad1af6
Ammar Sajjid <22f2000713@ds.study.iitm.ac.in>
2023-12-23 15:28:30 +0000

    added script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Export the GitHub username
-export GITHUB_USERNAME="<username>"
+export GITHUB_USERNAME="importAmmar"
 
 # Export the GitHub repository name
-export GITHUB_REPOSITORY="se2001-<roll_number>"
+export GITHUB_REPOSITORY="se2001-22f2000713"
